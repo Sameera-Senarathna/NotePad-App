51ab1f45e08b157442cbaa727285e75d9b81458b
Sameera Senarathna <s.s.f.senarathna@gmail.com>
2019-01-15 17:15:33 +0530

    Line item can added

--- js/myScript.js
+++ js/myScript.js
@@ -1,1 +1,20 @@
-$("#items").
+$("#items").keydown(function (event) {
+    if(event.keyCode==13){
+        var enteredText = $(this).val();
+        var append = `<div class="row">
+                        <div class="col-1 smallBtn noPadding">
+                            <span class="input-group-text onlyLeftPadding"><i class="fas fa-plus"></i></span>
+                        </div>
+                        <div class="col-10 noPadding input-group-sm">
+                            <input value="${enteredText}" type="text" class="form-control inputNoBoarder noPadding" disabled>
+                        </div>
+                        <div class="col-1">
+                            <span class="input-group-text"><i class="fas fa-plus"></i></span>
+                        </div>
+                    </div>`;
+
+        $("#lineitemlist").append(append);
+        $(this).val("");
+        console.log("The Word You Enter is :"+enteredText);
+    }
+});
