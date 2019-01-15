4c43327f54dbe1e99b343b89ba51ade5ef4a4806
Sameera Senarathna <s.s.f.senarathna@gmail.com>
2019-01-15 17:58:30 +0530

    input are completed

--- js/myScript.js
+++ js/myScript.js
@@ -3,13 +3,13 @@ $("#items").keydown(function (event) {
         var enteredText = $(this).val();
         var append = `<div class="row">
                         <div class="col-1 smallBtn noPadding">
-                            <span class="input-group-text onlyLeftPadding"><i class="fas fa-plus"></i></span>
+                            <span class="input-group-text onlyLeftPadding"><input type="checkbox" aria-label="Checkbox for following text input"></span>
                         </div>
                         <div class="col-10 noPadding input-group-sm">
-                            <input value="${enteredText}" type="text" class="form-control inputNoBoarder noPadding" disabled>
+                            <input value="${enteredText}" type="text" class="form-control inputNoBoarder noPadding">
                         </div>
                         <div class="col-1">
-                            <span class="input-group-text"><i class="fas fa-plus"></i></span>
+                            <span class="input-group-text smallBtn noPadding"><i class="fas fa-times"></i></span>
                         </div>
                     </div>`;
 
